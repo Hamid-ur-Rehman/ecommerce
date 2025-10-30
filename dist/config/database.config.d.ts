175da0aa0c26e33be1ad5044cfa7e23237eb1f94
Hamid ur rehman <73439528+Hamid-ur-Rehman@users.noreply.github.com>
2025-10-30 08:22:30 -0700

    remove dist folder

--- dist/config/database.config.d.ts
+++
@@ -1,4 +0,0 @@
-import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
-export declare class DatabaseConfig implements TypeOrmOptionsFactory {
-    createTypeOrmOptions(): TypeOrmModuleOptions;
-}
--- dist/database/run-seeder.js
+++
@@ -1,2 +0,0 @@
-console.log('Database seeder is disabled. Use npm run seed to run it manually.');
-//# sourceMappingURL=run-seeder.js.map
